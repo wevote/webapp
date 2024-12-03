d89100c67c48607a061a6e864b92e497680912c4
nadiavaida <nadiavaida@gmail.com>
2024-12-03 14:27:13 -0800

    Update RepresentativeListRoot.jsx - updated came casing for Id

--- src/js/components/RepresentativeListRoot/RepresentativeListRoot.jsx
+++ src/js/components/RepresentativeListRoot/RepresentativeListRoot.jsx
@@ -403,7 +403,7 @@ class RepresentativeListRoot extends Component {
               titleTextForList.length &&
               representativeList) &&
           (
-            <WhatIsHappeningTitle id="WhatIsHappeningTitle">
+            <WhatIsHappeningTitle id="whatIsHappeningTitle">
               {titleTextForList}
             </WhatIsHappeningTitle>
           )}
