34ad036f8fec8be26f30e4c377294406f2dc3d6a
Almee98 <almeechristian6198@gmail.com>
2024-12-12 08:47:12 -0500

    [Issue WV-701 & WV-585] Added breakpoints in iconButtonRoot and iconButtonRootSelected classes for different screen sizes, thus  aligning the notification icon with profile avatar icon.

--- src/js/components/Navigation/HeaderNotificationMenu.jsx
+++ src/js/components/Navigation/HeaderNotificationMenu.jsx
@@ -278,8 +278,9 @@ class HeaderNotificationMenu extends Component {
           id="headerNotificationMenuIcon"
           onClick={this.handleClick}
           size="large"
-          sx={isTablet() ? { marginTop: '5px', marginRight: '12px' } : {}}
-        >
+          // 'sx' refactored in the iconButtonRoot class below
+          // sx={isTablet() ? { marginTop: '5px', marginRight: '12px' } : {}}
+        > 
           {allActivityNoticesNotSeenCount ? (
             <Badge
               badgeContent={<BadgeCountWrapper>{allActivityNoticesNotSeenCount}</BadgeCountWrapper>}
@@ -343,6 +344,16 @@ const styles = (theme) => ({
     paddingRight: 0,
     paddingBottom: '9px !important',
     paddingLeft: 6,
+    [theme.breakpoints.down('sm')]: {
+      marginTop: '5px !important',
+    },
+    [theme.breakpoints.between('sm', 'md')]: {
+      marginTop: '4px !important',
+      marginRight: '12px',
+    },
+    [theme.breakpoints.up('md')]: {
+      marginTop: '3px !important',
+    },
     '&:hover': {
       backgroundColor: 'transparent',
     },
@@ -355,6 +366,16 @@ const styles = (theme) => ({
     paddingRight: 0,
     paddingBottom: '9px !important',
     paddingLeft: 6,
+    [theme.breakpoints.down('sm')]: {
+      marginTop: '5px !important',
+    },
+    [theme.breakpoints.between('sm', 'md')]: {
+      marginTop: '4px !important',
+      marginRight: '12px',
+    },
+    [theme.breakpoints.up('md')]: {
+      marginTop: '3px !important',
+    },
     '&:hover': {
       backgroundColor: 'transparent',
     },
